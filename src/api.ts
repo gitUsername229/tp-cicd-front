9ac279016f232e1b8fd0079febee71d678eb2b1a
didierdelaferme-ui <didier.delaferme@gmail.com>
2025-11-13 15:24:05 +0100

    Update API base URL to include port 443

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://tp-ci-cd-master.onrender.com', // will call relative endpoints like /authors, /books, etc.
+  baseURL: 'https://tp-ci-cd-master.onrender.com:443', // will call relative endpoints like /authors, /books, etc.
 });
 
 export default api;
